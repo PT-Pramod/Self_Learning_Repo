7d9a84c3823313a71a3e3a15ac25a8389a430b35
Pramod <tiwari.pramod17@gmail.com>
2023-05-04 18:50:34 +0530

    Pushing the change in student class

--- Java_Learning/src/classroom_program/Student.java
+++ Java_Learning/src/classroom_program/Student.java
@@ -1,8 +1,11 @@
 /*For Printing Use Sysout and Ctrl + Space*/
 
 package classroom_program; //Package created
+import inheritance.Maruti;
+/* import the inheritance package to use the Maruti class as example of inheritance in different package 
+ * so that i can use variable and method of that class in this class */
 
-public class Student //Class created
+public class Student extends Maruti //Class created
 {
 	int a; //Declaring the variable
 	public void Pramod() //Creating the method 
@@ -15,7 +18,7 @@ public static void main (String[]args) // main method
 	Student abc=new Student(); //creating the object
 	abc.Pramod(); // Calling a method
 	abc.a = 150 ; //called the variable and assigned the value
+	abc.m1();
 	System.out.println(abc.a);
-	}	
+	}
 }
-
