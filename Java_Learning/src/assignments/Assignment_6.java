5e7bb762933ab82ddb81864182481b303acffac5
Pramod <tiwari.pramod17@gmail.com>
2023-05-08 11:17:18 +0530

    Pushing Assignment 6

--- Java_Learning/src/assignments/Assignment_6.java
+++ Java_Learning/src/assignments/Assignment_6.java
@@ -12,5 +12,30 @@ package assignments;
 
 public class Assignment_6 extends Assignment_3
 {
-
+ public Assignment_6 () 
+ {
+	 System.out.println("Child default constructor");
+ }
+ public Assignment_6 (int a) 
+ {
+	 this (10,20);
+	 System.out.println("Child 1 parameterized constructor");
+ }
+ public Assignment_6 (int a, int b) 
+ {
+	 super (10);
+	 System.out.println("Child 2 parameterized constructor");
+ }
+ public Assignment_6 (int a, int b, int c) 
+ {
+	 this ();
+	 System.out.println("Child 3 parameterized constructor");
+ }
+ 
+ public static void main(String[] args) 
+ {
+	 Assignment_6 as=new Assignment_6(10);
+	 
+ }
+ 
 }
